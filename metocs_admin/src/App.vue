15303a7bc1f735d953e7f3542b0bccdb8dd23e82
metocs <164857362@qq.com>
2024-01-20 12:58:36 +0800

    第一次提交后台Vue代码：实现第一个App.vue文件

---
+++ metocs_admin/src/App.vue
@@ -0,0 +1,29 @@
+
+<!-- .vue 文件可以编写以下三种标签-->
+
+<!--html-->
+<template>
+  <div class="hello">
+    <h1>Hello World！</h1>
+  </div>
+</template>
+
+
+<!--逻辑-->
+<script lang="ts">
+  export default {
+    name: "App"
+  }
+</script>
+
+
+<!--样式-->
+
+<style>
+   .hello{
+     background-color: aquamarine;
+     box-shadow: 0 0 10px;
+     border-radius: 10px;
+     padding: 20px;
+   }
+</style>
